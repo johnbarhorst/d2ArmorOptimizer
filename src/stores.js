2126800b0c33747ef5423690858c50c1dc2e80bf
John Barhorst <crashdsinc@comcast.net>
2019-11-06 20:52:55 -0600

    stores

--- src/stores.js
+++ src/stores.js
@@ -1,12 +1,22 @@
 import { writable } from 'svelte/store';
 import API_KEY from '../API_KEY.js';
 
-export let currentMembershipId = writable(0);
-export let accountNameSearched = writable(false);
 
+// API/PROFILE STUFF
+export let currentMembershipId = writable(0);
+export let characterHashArray = writable([]);
+export let membershipType = writable('All');
+
+
+
+// FLAGS AND STATE TRACKING
+export let accountNameSearched = writable(false);
+export let accountSelected = writable(false);
+
+// NOT SURE IF THIS EVEN NEEDS TO EXIST BUT THERE MIGHT BE OTHER THINGS LATER
 export const platformPath = 'https://www.bungie.net/Platform';
 
-
+// GLOBAL FUNCTIONS
 export const apiCall = async function (apiPath) {
   const response = await fetch(
     `${platformPath}${apiPath}`,
