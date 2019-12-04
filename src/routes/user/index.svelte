8d5fc006ab10fa151c0883f43e50c3798a86e545
John Barhorst <crashdsinc@comcast.net>
2019-12-03 18:59:10 -0600

    start of promised loading on user page

--- src/routes/user/index.svelte
+++ src/routes/user/index.svelte
@@ -1,15 +1,13 @@
 <script>
   import { onMount } from "svelte";
-  const getUser = async function() {
+  const getCurrentUser = async function() {
     const response = await fetch(`/api/User/getUser.json`);
     const data = await response.json();
     console.log("Data in user route index");
     console.log(data);
+    return data;
   };
-
-  onMount(() => {
-    getUser();
-  });
+  const profile = getCurrentUser();
 </script>
 
 <style>
@@ -17,3 +15,11 @@
 </style>
 
 <h1>User Page</h1>
+
+{#await profile}
+  <h2>Loading</h2>
+{:then profile}
+  <h2>{profile.username}</h2>
+{:catch error}
+  <h2>Error</h2>
+{/await}
