c05fb27e3a1ec7feeb3f2067f97107757b642cd2
John Barhorst <crashdsinc@comcast.net>
2019-12-04 19:24:42 -0600

    User page checks for a user

--- src/routes/api/User/getUser.json.js
+++ src/routes/api/User/getUser.json.js
@@ -1,12 +1,10 @@
 import { User } from '../../../server';
 
 export async function get(req, res, next) {
-  const userId = req.session.userId;
-  const data = await User.findById(userId);
+  const data = await User.findById(req.session.userId);
   const userData = JSON.stringify(data);
   console.log('Response from getUser');
   console.log(data);
   console.log(userData);
   res.send(userData);
-
 }
--- src/routes/user/index.svelte
+++ src/routes/user/index.svelte
@@ -1,5 +1,6 @@
 <script>
-  import { onMount } from "svelte";
+  import { stores } from "@sapper/app";
+  const { preloading, page, session } = stores();
   const getCurrentUser = async function() {
     const response = await fetch(`/api/User/getUser.json`);
     const data = await response.json();
@@ -16,10 +17,15 @@
 
 <h1>User Page</h1>
 
-{#await profile}
-  <h2>Loading</h2>
-{:then profile}
-  <h2>{profile.username}</h2>
-{:catch error}
-  <h2>Error</h2>
-{/await}
+{#if $session.user}
+  {#await profile}
+    <h2>Loading</h2>
+  {:then profile}
+    <h2>{profile.username}</h2>
+  {:catch error}
+    <h2>Error</h2>
+  {/await}
+{:else}
+  <!-- TODO: Cmon, you can do better than this! -->
+  <h1>You'll have to log in!</h1>
+{/if}
